1d901db1172c6d1a06fe878ae5324810cc2a1b1b
G G <ggauthier.ggl@gmail.com>
2011-06-22 16:38:23 +0200

    D?but de codage de DownloadManager.

--- dev/NouvelleVersion/src/core/DownloadManager.py
+++ dev/NouvelleVersion/src/core/DownloadManager.py
@@ -1,8 +1,55 @@
+# -*- coding:Utf-8 -*-
 
-class DownloadManager :
+import thread,threading
+
+import logging
+logger = logging.getLogger( __name__ )
+
+class DownloadManager(threading.Thread):
+	BUFFER_SIZE = 8000
+	
 	def __init__(self) :
+		threading.Thread.__init__(self)
 		self.nextNumDownload = 0 # int
-		pass
+		
+		self.dlFactory = DownloaderFactory();
+		
+		self.toDl = []
+		self.mutex_toDl = thread.allocate_lock()
+		self.cond_toDl = threading.Condition(self.mutex_toDl)
+		
+		self.start()
+	
+	def run(self):
+		while True:
+			if self.mutex_toDl.locked():
+				logger.warn("Mutex verrouillé, attention au dead lock !")
+			self.mutex_toDl.acquire()
+			while len(self.toDl) <= 0:
+				self.cond_toDl.wait();
+				if self.mutex_toDl.locked():
+					logger.warn("Mutex verrouillé, attention au dead lock !")
+				self.mutex_toDl.acquire()
+			dlParam = self.toDl.pop(0)
+			self.mutex_toDl.release()
+			
+			print "Téléchargement de 3"+dlParam[0]+"..."
+			dler = self.getDownloader()
+			try:
+				outfile = open(dlParam[1], "w")
+				dler.start()
+				carac = dler.read(DownloadManager.BUFFER_SIZE)
+				while len(carac) > 0:
+					oufile.write(carac)
+					carac = dler.read(DownloadManager.BUFFER_SIZE)
+				outfile.close()
+				dler.stop()
+			except:
+				print "Erreur de téléchargement !"
+	
+	def getDownloader(self, url):
+		#return self.dlFactory.create(....)
+		return HttpDownloader(url);
 	
 	## Lance ou met en attente un téléchargement.
 	# @param url l'url du fichier distant
@@ -10,6 +57,11 @@ class DownloadManager :
 	# @param callback le DownloadCallback à tenir informé de l'état du téléchargement
 	# @return le numéro du téléchargement
 	def download (self, url, outFile, callback) :
-		# returns int
+		if self.mutex_toDl.locked():
+			logger.warn("Mutex verrouillé, attention au dead lock !")
+		self.mutex_toDl.acquire()
+		self.toDl.append([url, outFile, callBack])
+		self.cond_toDl.notifyAll()
+		self.mutex_toDl.release()
 		pass
 
--- dev/NouvelleVersion/src/core/HttpDownloader.py
+++ dev/NouvelleVersion/src/core/HttpDownloader.py
@@ -1,13 +1,25 @@
+# -*- coding:Utf-8 -*-
+
+import urllib
 
 class HttpDownloader (DownloaderInterface) :
-	def __init__(self) :
-		pass
-	def HttpDownloader (self, url) :
-		# returns 
-		pass
+	def __init__(self, url) :
+		DownloaderInterface.__init__(self)
+		self.url = url
+	
+	def start(self):
+		parsed = urlparse.urlparse(self.url)
+		httpcon = httplib.HTTPConnection(parsed.netloc)
+		httpcon.request("GET", parsed.path+"?"+parsed.query)
+		resp.httpcon.getresponse()
+		self.stream = resp
+	
 	def read (self, n) :
-		# returns byte[]
-		pass
+		return self.stream.read(n)
+	
+	def stop(self):
+		self.stream.close()
+	
 	def canDownload (self, url) :
 		# returns bool
 		pass
