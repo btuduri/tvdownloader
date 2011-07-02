4938074a2d4b05c1b4343cdb73b31c3134bb2ece
G G <ggauthier.ggl@gmail.com>
2011-07-02 12:25:48 +0200

    Nettoyage oubli?.

--- dev/NouvelleVersion/src/core/FtpDownloader.py
+++ dev/NouvelleVersion/src/core/FtpDownloader.py
@@ -19,46 +19,12 @@ class FtpDownloader (DownloaderInterface) :
 			ftpconn = ftplib.FTP(parsed.netloc)
 			ftpconn.login()
 			
-			
-			
-#			res = re.findall("[0-9]+", ftpconn.sendcmd("PASV"))
-#			if len(res) != 7:
-#				self.size = 0
-#				return False
-#			ip = res[1]+"."+res[2]+"."+res[3]+"."+res[4]
-#			port = int(res[5])*256 + int(res[6])
-#			sockconn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#			ftpconn.sendcmd("LIST "+parsed.path)
-#			sockconn.connect((ip, port))
-#			ftplist = sockconn.recv(800)
-#			sockconn.close()
-#			self.size = int(re.match("[^\t ]+[\t ]+[^\t ]+[\t ]+[^\t ]+[\t ]+[^\t ]+[\t ]+([0-9]+)", ftplist).group(1))
-#			print "taille:",self.size
-#			
-#			ftpconn.sock.recv(800)
-#			s = ftpconn.sendcmd("PASV")
-#			res = re.findall("[0-9]+", s)
-#			if len(res) != 7:
-#				print s
-#				self.size = 0
-#				return False
-#			ip = res[1]+"."+res[2]+"."+res[3]+"."+res[4]
-#			port = int(res[5])*256 + int(res[6])
-#			sockconn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#			sockconn.connect((ip, port))
-#			print sockconn.fileno()
-#			self.stream = os.fdopen(sockconn.fileno())
-#			
-#			ftpconn.sendcmd("RETR "+parsed.path)
-			
 			self.stream,self.size = ftpconn.ntransfercmd("RETR "+parsed.path)
 			
 			self.ftpconn = ftpconn
 		except BaseException as e:
 			import traceback
 			traceback.print_exc(e)
-			if self.ftpconn != None:
-				self.ftpconn.close()
 			if self.stream != None:
 				self.stream.close()
 			return False
