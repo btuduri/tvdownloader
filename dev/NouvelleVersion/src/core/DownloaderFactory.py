4d5e3748b1c011251f0805724e2e424cf893bb12
G G <ggauthier.ggl@gmail.com>
2011-07-09 16:15:28 +0200

    Oups :/.

--- dev/NouvelleVersion/src/core/DownloaderFactory.py
+++ dev/NouvelleVersion/src/core/DownloaderFactory.py
@@ -14,11 +14,12 @@ class DownloaderFactory (AbstractDownloaderFactory) :
 	# @param url l'url du fichier à télécharger
 	# @return un DownloaderInterface permettant le téléchargement du fichier distant ou None si aucun compatible
 	def create (self, url) :
-		return MsdlDownloader(url)
 		if url[:4] == "ftp:":
 			return FtpDownloader(url)
 		elif url[:5] == "http:":
 			return HttpDownloader(url)
+		elif url[:4] == "mms:":
+			return MsdlDownloader(url)
 		else:
 			return None
 
