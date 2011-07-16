ad0a5e62e51d53f419717cf87dce536fef0165f4
chaoswizard <chaoswizard@gmail.com>
2011-07-16 16:38:17 +0200

    Ajouts de champs dans plugin.

--- dev/NouvelleVersion/src/core/Plugin.py
+++ dev/NouvelleVersion/src/core/Plugin.py
@@ -5,14 +5,26 @@
 # Modules #
 ###########
 
-
+from PluginCache import PluginCache
 
 ##########
 # Classe #
 ##########
 
 class Plugin :
-	def __init__( self, nom, nomComplet ) :
-		self.nom        = nom        # Nom du plugin (sans accents, espaces, caracteres speciaux, ...)
-		self.nomComplet = nomComplet # Nom complet du plugin
-
+	
+	def __init__( self, nom, nomComplet, nomImage ) :
+		self.nom        = nom                      # Nom du plugin (sans accents, espaces, caracteres speciaux, ...)
+		self.nomComplet = nomComplet               # Nom complet du plugin
+		self.image      = chargerImage( nomImage ) # Image representant le plugin
+		
+		# Cache
+		self.cache      = PluginCache()
+		# S'ajoute soit meme au cache
+		self.cache.ajouterPlugin( self.nom )
+	
+	## Methode qui charge l'image du plugin
+	# @param nomImage Nom de l'image a charger
+	# @return         Image
+	def chargerImage( self, nomImage ):
+		pass
---
+++ dev/NouvelleVersion/src/plugins/CanalPlus.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding:Utf-8 -*-
+
+###########
+# Modules #
+###########
+
+from Plugin import Plugin
+
+###########
+# Classes #
+###########
+
+class CanalPlus( Plugin ):
+	
+	def __init__( self ):
+		Plugin.__init__( self, "CanalPlus", "Canal +", "CanalPlus.jpg" )
