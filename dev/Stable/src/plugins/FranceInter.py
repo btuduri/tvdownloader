ca3cdd892ffbe65c5a76a43ffba6b9850c98c0cd
chaoswizard <chaoswizard@gmail.com>
2011-11-30 12:07:09 +0100

    Correction FranceInter.

--- dev/Stable/src/plugins/FranceInter.py
+++ dev/Stable/src/plugins/FranceInter.py
@@ -30,17 +30,12 @@ from Plugin import Plugin
 
 class FranceInter( Plugin ):
 	
-	urlListesEmissions = [ [ "Du lundi au vendredi", "http://sites.radiofrance.fr/franceinter/pod/index.php?page=chrono&jd=sem" ],
-						   [ "Le samedi", "http://sites.radiofrance.fr/franceinter/pod/index.php?page=chrono&jd=sam" ],
-						   [ "Le dimanche", "http://sites.radiofrance.fr/franceinter/pod/index.php?page=chrono&jd=dim" ]
-						 ]
-	listeEmissions       = {} # { Nom chaine : { Nom emission : URL fichier XML } }
-	urlPageDescriptive   = "http://sites.radiofrance.fr/_c/php/popcast.php?chaine=1&cid="
-	pageDescriptiveRegEx = re.compile( '<h2>(.+?)</h2>.+?<p class="rssLink">(.+?)</p>' , re.DOTALL ) 
-	derniereChaine = ""
+	ulrListeEmission     = "http://www.franceinter.fr/podcasts/liste-des-emissions"
+	listeEmissions       = {} # { Nom emission, URL fichier XML }
+	pageRegEx            = re.compile( 'class="visuel" title="([^"]*?)".+?href="(http://radiofrance-podcast.net/podcast09/.+?.xml)', re.DOTALL )
 	
 	def __init__( self ):
-		Plugin.__init__( self, "France Inter", "http://sites.radiofrance.fr/franceinter/accueil/", 30 )
+		Plugin.__init__( self, "France Inter", "http://www.franceinter.fr/podcasts", 30 )
 		
 		if os.path.exists( self.fichierCache ):
 			self.listeEmissions = self.chargerCache()
@@ -50,57 +45,36 @@ class FranceInter( Plugin ):
 		
 		# On remet a 0 la liste des emissions
 		self.listeEmissions.clear()
-		
-		# Pour chaque page
-		for ( nom, urlPage ) in self.urlListesEmissions:
-			# On cree la chaine dans la liste des programmes
-			self.listeEmissions[ nom ] = {}
-			# On recupere la page web
-			page = self.API.getPage( urlPage )
-			# On extrait les numeros de chacune des pages des podcasts
-			listeNumeros = re.findall( '<h6><a href="javascript:popcast\(([0-9]+?), 1\);"', page )
-			# On cree la liste de toutes les pages que l'on doit recuperer
-			listeURL = []
-			for numero in listeNumeros:
-				listeURL.append( self.urlPageDescriptive + numero )
-			# On recupere toutes ces pages
-			listePages = self.API.getPages( listeURL )
-			# Pour chaque page = emission
-			for pageEmission in listePages.values():
-				# On recupere les informations
-				infos = re.findall( self.pageDescriptiveRegEx, pageEmission )
-				for ( nomEmission, urlFichierXML ) in infos:
-					nomEmission = nomEmission.replace( "&raquo;", "" ).title()
-					self.listeEmissions[ nom ][ nomEmission ] = urlFichierXML
+		# On recupere la page web
+		page = self.API.getPage( self.ulrListeEmission )
+		# Extraction des emissions
+		self.listeEmissions = dict( re.findall( self.pageRegEx, page ) )
 		
 		self.sauvegarderCache( self.listeEmissions )
 		self.afficher( u"Liste des émissions sauvegardées" )
 		
 	def listerChaines( self ):
-		for chaine in self.listeEmissions.keys():
-			self.ajouterChaine( chaine )
+		self.ajouterChaine( self.nom )
 	
 	def listerEmissions( self, chaine ):
-		if( self.listeEmissions.has_key( chaine ) ):
-			self.derniereChaine = chaine
-			for emission in self.listeEmissions[ chaine ].keys():
-				self.ajouterEmission( chaine, emission )
+		for emission in self.listeEmissions.keys():
+			self.ajouterEmission( chaine, emission )
 				
 	def listerFichiers( self, emission ):
-		if( self.listeEmissions.has_key( self.derniereChaine ) ):
-			if( self.listeEmissions[ self.derniereChaine ].has_key( emission ) ):
-				# On recupere la page web qui liste les fichiers
-				pageXML = self.API.getPage( self.listeEmissions[ self.derniereChaine ][ emission ] )
-				
-				# On extrait les fichiers
-				listeFichiers = []
-				handler = PodcastsHandler( listeFichiers )
-				try:
-					xml.sax.parseString( pageXML, handler )
-				except:
-					return
-				
-				# On ajoute les fichiers
-				for fichier in listeFichiers:
-					self.ajouterFichier( emission, fichier )
+		
+		if( self.listeEmissions.has_key( emission ) ):
+			# On recupere la page web qui liste les fichiers
+			pageXML = self.API.getPage( self.listeEmissions[ emission ] )
+			
+			# On extrait les fichiers
+			listeFichiers = []
+			handler = PodcastsHandler( listeFichiers )
+			try:
+				xml.sax.parseString( pageXML, handler )
+			except:
+				return
+			
+			# On ajoute les fichiers
+			for fichier in listeFichiers:
+				self.ajouterFichier( emission, fichier )
 		
