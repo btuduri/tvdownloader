4a9f0fa195e8f3077932de2ecd5908aebc08b51a
G G <ggauthier.ggl@gmail.com>
2011-12-29 23:09:34 +0100

    Mode distribu? OK, lancer une UI avec l'option --shared apr?s avoir lanc? le d?mon.

--- dev/core/__init__.py
+++ dev/core/__init__.py
@@ -25,20 +25,40 @@ import sys
 
 STATUS_NOT_INITIALIZED = "non initialisé"
 STATUS_INITIALIZED = "initialisé"
-STATUS_DAEMON_NOT_FOUND = "démon introuvable"
+STATUS_SHARED_OBJECT_ERROR = "erreur avec les objets distribués"
 
 STATUS = STATUS_NOT_INITIALIZED
 
-ARG_SHARED_OBJECT = "--client"
+ARG_SHARED_OBJECT = "--shared"
+SHARED_OBJECT_AUTHKEY = "tvd"
+SHARED_OBJECT_ADDRESS = ('', 5096)
 
 PLUGIN_MANAGER = None
 DOWNLOAD_MANAGER = None
 HISTORIQUE = None
 
 def initialiser():
+	global STATUS,PLUGIN_MANAGER,DOWNLOAD_MANAGER,HISTORIQUE
 	if ARG_SHARED_OBJECT in sys.argv[1:]:
 		#TODO Récupérer les stubs
-		a = 0
+		import multiprocessing
+		from multiprocessing.managers import BaseManager
+
+		BaseManager.register('pluginManager')
+		BaseManager.register('downloadManager')
+		BaseManager.register('historique')
+
+		manager = BaseManager(SHARED_OBJECT_ADDRESS, SHARED_OBJECT_AUTHKEY)
+		try:
+			manager.connect()
+			
+			PLUGIN_MANAGER = manager.pluginManager()
+			DOWNLOAD_MANAGER = manager.downloadManager()
+			HISTORIQUE = manager.historique()
+			STATUS = STATUS_INITIALIZED
+		except Exception as e:
+			print e
+			STATUS = STATUS_SHARED_OBJECT_ERROR
 	else:
 		PLUGIN_MANAGER = PluginManager()
 		DOWNLOAD_MANAGER = DownloadManager()
--- dev/other/daemon/tvdownloader-daemon.py
+++ dev/other/daemon/tvdownloader-daemon.py
@@ -6,5 +6,18 @@ import core,sys
 if core.initialiser() != core.STATUS_INITIALIZED:
 	print "Erreur d'initialisation du module core"
 	sys.exit(1)
-print "tvdd lancé (TODO)"
-#Lancé le démon (distribution des objets)
+else:
+	print core.STATUS
+
+import multiprocessing
+from multiprocessing.managers import BaseManager
+
+BaseManager.register('pluginManager', callable=lambda:core.PLUGIN_MANAGER)
+BaseManager.register('downloadManager', callable=lambda:core.DOWNLOAD_MANAGER)
+BaseManager.register('historique', callable=lambda:core.HISTORIQUE)
+
+manager = BaseManager(core.SHARED_OBJECT_ADDRESS, core.SHARED_OBJECT_AUTHKEY)
+
+server = manager.get_server()
+print "Démon lancé..."
+server.serve_forever()
