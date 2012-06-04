994f05591ca7e0c219d73e636669172ed8c2060a
chaoswizard <chaoswizard@gmail.com>
2012-06-04 20:32:18 +0200

    Correction.

--- other/Stable/src/plugins/PublicSenat.py
+++ other/Stable/src/plugins/PublicSenat.py
@@ -18,7 +18,6 @@ from Plugin import Plugin
 class PublicSenat( Plugin ):
 	
 	pagePrincipale = "http://www.publicsenat.fr"
-	
 	# { Nom emission, id de l'emission sur le site }
 	listeEmissions = { r"A l'heure du choix"       : 270,
 					   r"A nous le Sénat"          : 281,
@@ -45,20 +44,21 @@ class PublicSenat( Plugin ):
 		# TODO : parcourir toutes les pages, recuperer la date et la description
 		#
 		
-		# Recupere la page qui liste les fichiers disponibles
-		pageFichiers = BeautifulSoup.BeautifulSoup( self.API.getPage( "http://www.publicsenat.fr/zp/templates/emission/JX_video.php", { "idP" : self.listeEmissions[ emission ], "page" : 1 } ) )
-		# Recupere les fichiers disponibles
-		emissionsData = pageFichiers.findAll( "h3" )
-		for emissionData in emissionsData:
-			try:
-				nom     = emissionData.contents[ 0 ].contents[ 0 ]
-				urlPage = "http://www.publicsenat.fr%s" %( emissionData.contents[ 0 ][ "href" ] )
-				# Recupere des infos sur la page du fichier
-				page = BeautifulSoup.BeautifulSoup( self.API.getPage( urlPage ) )
-				# Extrait les donnees
-				urlPhoto  = "http://www.publicsenat.fr%s" %( page.findAll( "input", { "id" : "imgEmissionSelect" } )[ 0 ][ "value" ] )
-				lienVideo = page.findAll( "input", { "id" : "flvEmissionSelect" } )[ 0 ][ "value" ]
-				# Ajouter le fichier
-				self.ajouterFichier( emission, Fichier( nom = nom, lien = lienVideo, urlImage = urlPhoto ) )
-			except:
-				continue
+		if( self.listeEmissions.has_key( emission ) ):
+			# Recupere la page qui liste les fichiers disponibles
+			pageFichiers = BeautifulSoup.BeautifulSoup( self.API.getPage( "http://www.publicsenat.fr/zp/templates/emission/JX_video.php", { "idP" : self.listeEmissions[ emission ], "page" : 1 } ) )
+			# Recupere les fichiers disponibles
+			emissionsData = pageFichiers.findAll( "h3" )
+			for emissionData in emissionsData:
+				try:
+					nom     = emissionData.contents[ 0 ].contents[ 0 ]
+					urlPage = "http://www.publicsenat.fr%s" %( emissionData.contents[ 0 ][ "href" ] )
+					# Recupere des infos sur la page du fichier
+					page = BeautifulSoup.BeautifulSoup( self.API.getPage( urlPage ) )
+					# Extrait les donnees
+					urlPhoto  = "http://www.publicsenat.fr%s" %( page.findAll( "input", { "id" : "imgEmissionSelect" } )[ 0 ][ "value" ] )
+					lienVideo = page.findAll( "input", { "id" : "flvEmissionSelect" } )[ 0 ][ "value" ]
+					# Ajouter le fichier
+					self.ajouterFichier( emission, Fichier( nom = nom, lien = lienVideo, urlImage = urlPhoto ) )
+				except:
+					continue
